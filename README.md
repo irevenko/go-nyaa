cff2c54b5a72c77dc9e10169b65ec85b229eff85
irevenko <kekalkaru20@gmail.com>
2021-04-01 14:53:36 +0300

    Update url.go README

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # 🐈📦 go-nyaa
 
+[![Go Reference](https://pkg.go.dev/badge/github.com/irevenko/go-nyaa.svg)](https://pkg.go.dev/github.com/irevenko/go-nyaa)
+
 > nyaa.si client library for Go
 
 <p align="center"><img src="https://avatars.githubusercontent.com/u/28658394?s=200&v=4"></p>
@@ -17,46 +19,101 @@ Contributions, issues and feature requests are welcome! 👍 <br>
 Feel free to check [open issues](https://github.com/irevenko/go-nyaa/issues).
 
 # Docs 📋
-Go Reference: https://pkg.go.dev/github.com/irevenko/go-nyaa
+<a href="https://pkg.go.dev/github.com/irevenko/go-nyaa">Go reference</a>
+
+## Search Example
 
 ```go
 import ( 
     "fmt"
+    "log"
 
 	"github.com/irevenko/go-nyaa/nyaa"
 )
 
 func main() {
-	opt := nyaa.SearchOptions{
-		Query:    "LN",
-		Category: "literature",
-		SortBy:   "seeders",
+    opt := nyaa.SearchOptions{
+        Query:    "LN",
+        Category: "literature",
+        SortBy:   "seeders",
         Filter:   "trusted-only",
-	}
+    }
 
-	torrents, err := nyaa.Search(opt)
-	if err != nil {
-		log.Fatal(err)
-	}
+    torrents, err := nyaa.Search(opt)
+    if err != nil {
+        log.Fatal(err)
+    }
 
-	fmt.Println(torrents)
+    fmt.Println(torrents)
 }
 ```
+
+## SearchOptions
+```go
+type SearchOptions struct {
+	Query    string
+	Category string
+	SortBy   string
+	Filter   string
+}
+```
+## Query
+- your desired search string
+
+## Category
+* ```all``` - All Categories
+* ```anime``` - All Anime
+    * ```anime-amv``` 
+    * ```anime-eng```
+    * ```anime-non-eng```
+    * ```anime-raw```
+* ```audio``` - All Audio
+    * ```audio-lossless``` 
+    * ```audio-lossy```
+* ```literature``` - All Literature
+    * ```literature-eng``` 
+    * ```literature-non-eng```
+    * ```literature-raw``` 
+* ```live-action``` - All Live Action
+    * ```live-action-idol-prom``` 
+    * ```live-action-eng```
+    * ```live-action-non-eng```
+    * ```live-action-raw```
+* ```pictures``` - All Pictures
+    * ```pictures-graphics``` 
+    * ```pictures-photos``` 
+* ```software``` - All Software
+    * ```software-apps``` 
+    * ```software-games```
+
+## SortBy
+- ```comments```
+- ```downloads``` 
+- ```date``` 
+- ```seeders``` 
+- ```leechers```
+- ```size``` 
+
+## Filter
+- ```no-filter```
+- ```no-remakes``` 
+- ```trusted-only``` 
+
 
 # Notes
 - Pagination does not work with RSS
 - Ascending sort does not work with RSS
 
 # ToDo
-- [x] Sort by comments, size, date, seeders, leechers,  
-- [x] Filters: No filters, No Remakes, Trusted only 
-- [x] Search Home Page (basically all newest torrents)
+- [x] Sort by comments, size, date, seeders, leechers, downloads
+- [x] Filter: No filter, No remakes, Trusted only 
+- [x] Search All
 - [x] Search Anime
-- [x] Search Literature (Mostly Mangas,LNs)
+- [x] Search Literature
 - [x] Search Audio
 - [x] Search Live Action
 - [x] Search Pictures
-- [x] Search Software (apps, games)
+- [x] Search Software
 - [ ] Add sukebei.nyaa.si support
 - [ ] Scrap full description and comments from nyaa.si/view/...
 
--- nyaa/url.go
+++ nyaa/url.go
@@ -11,17 +11,40 @@ const (
 	sortBySize      = "&s=size&o=desc"
 	sortByDate      = "&s=id&o=desc"
 
-	filterNoFilters   = "&f=0"
+	filterNoFilter    = "&f=0"
 	filterNoRemakes   = "&f=1"
 	filterTrustedOnly = "&f=2"
 
-	categoryAll        = "&c=0_0"
-	categoryAnime      = "&c=1_0"
-	categoryAudio      = "&c=2_0"
-	categoryLiterature = "&c=3_0"
-	categoryLiveAction = "&c=4_0"
-	categoryPictures   = "&c=5_0"
-	categorySoftware   = "&c=6_0"
+	categoryAll = "&c=0_0"
+
+	categoryAnime       = "&c=1_0"
+	categoryAnimeAMV    = "&c=1_1"
+	categoryAnimeEng    = "&c=1_2"
+	categoryAnimeNonEng = "&c=1_3"
+	categoryAnimeRaw    = "&c=1_4"
+
+	categoryAudio         = "&c=2_0"
+	categoryAudioLossless = "&c=2_1"
+	categoryAudioLossy    = "&c=2_2"
+
+	categoryLiterature       = "&c=3_0"
+	categoryLiteratureEng    = "&c=3_1"
+	categoryLiteratureNonEng = "&c=3_2"
+	categoryLiteratureRaw    = "&c=3_3"
+
+	categoryLiveAction         = "&c=4_0"
+	categoryLiveActionRaw      = "&c=4_4"
+	categoryLiveActionEng      = "&c=4_1"
+	categoryLiveActionNonEng   = "&c=4_3"
+	categoryLiveActionIdolProm = "&c=4_2"
+
+	categoryPictures         = "&c=5_0"
+	categoryPicturesGraphics = "&c=5_1"
+	categoryPicturesPhotos   = "&c=5_2"
+
+	categorySoftware      = "&c=6_0"
+	categorySoftwareApps  = "&c=6_1"
+	categorySoftwareGames = "&c=6_2"
 )
 
 func buildURL(opts SearchOptions) (string, error) {
@@ -37,16 +60,50 @@ func buildURL(opts SearchOptions) (string, error) {
 			url += categoryAll
 		case "anime":
 			url += categoryAnime
+		case "anime-amv":
+			url += categoryAnimeAMV
+		case "anime-eng":
+			url += categoryAnimeEng
+		case "anime-non-eng":
+			url += categoryAnimeNonEng
+		case "anime-raw":
+			url += categoryAnimeRaw
 		case "audio":
 			url += categoryAudio
+		case "audio-lossless":
+			url += categoryAudioLossless
+		case "audio-lossy":
+			url += categoryAudioLossy
 		case "literature":
 			url += categoryLiterature
+		case "literature-eng":
+			url += categoryLiteratureEng
+		case "literature-non-eng":
+			url += categoryLiteratureNonEng
+		case "literature-raw":
+			url += categoryLiteratureRaw
 		case "live-action":
 			url += categoryLiveAction
+		case "live-action-raw":
+			url += categoryLiveActionRaw
+		case "live-action-eng":
+			url += categoryLiveActionEng
+		case "live-action-non-eng":
+			url += categoryLiveActionNonEng
+		case "live-action-idol-prom":
+			url += categoryLiveActionIdolProm
 		case "pictures":
 			url += categoryPictures
+		case "pictures-graphics":
+			url += categoryPicturesGraphics
+		case "pictures-photos":
+			url += categoryPicturesPhotos
 		case "software":
 			url += categorySoftware
+		case "software-apps":
+			url += categorySoftwareApps
+		case "software-games":
+			url += categorySoftwareGames
 		default:
 			err := fmt.Errorf("such category option does not exitst\nsee docs: https://github.com/irevenko/go-nyaa")
 			return "", err
@@ -75,8 +132,8 @@ func buildURL(opts SearchOptions) (string, error) {
 
 	if opts.Filter != "" {
 		switch opts.Filter {
-		case "no-filters":
-			url += filterNoFilters
+		case "no-filter":
+			url += filterNoFilter
 		case "no-remakes":
 			url += filterNoRemakes
 		case "trusted-only":
